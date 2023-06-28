10f4abd1f21e4fc64414a0ea20ae3b9f7e242411
Kevin Kipkemboi <theptjhub@gmail.com>
2023-06-27 17:52:43 -0700

    fixed registerModal height issue

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -69,13 +69,13 @@ const Modal: React.FC<ModalProps> = ({
 
   return (
     <>
-        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
+        <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
             <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
                 {/* content */}
                 <div className={`translate duration-300 h-full ${showModal ? 'translate-y-0' : 'translate-y-full'} ${showModal ? 'opacity-100' : 'opacity-0'}`}>
                     <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                         {/* header */}
-                        <div className="flex items-center p-6 rounded-t justify-center relative border-b">
+                        <div className="flex items-center p-3 rounded-t justify-center relative border-b">
                             <button className="p-1 border-0 hover:opacity-70 transition absolute left-9" onClick={handleClose}>
                                 <IoMdClose size={18} />
                             </button>
--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -59,7 +59,7 @@ const RegisterModal = () => {
     }, [registerModal, loginModal])
 
     const bodyContent = (
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-2">
             <Heading title="Welcome to Airbnb" subtitle="Create an account!"/>
             <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required />
             <Input id="name" label="Name" disabled={isLoading} register={register} errors={errors} required />
@@ -68,7 +68,7 @@ const RegisterModal = () => {
     )
 
     const footerContent = (
-        <div className="flex flex-col gap-4 mt-3">
+        <div className="flex flex-col gap-2 mt-2">
             <hr />
             <Button outline label="Continue with Google" icon={FcGoogle} onClick={() => signIn('google')} />
             <Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => signIn('github')} />
