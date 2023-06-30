fb2a3879e15d0ccac26184d06ba09de38fb0a689
Kevin Kipkemboi <theptjhub@gmail.com>
2023-06-29 17:23:47 -0700

    reduced logo size and added css

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -9,10 +9,10 @@ const Logo = () => {
   return (
     <Image 
     onClick={() => router.push('/')}
-    className="hidden md:block cursor-pointer"
+    className="hidden md:block cursor-pointer py-3 px-4 rounded-full hover:bg-neutral-100 transition"
     src="/images/kbnb_wide.png"
     height="100"
-    width="200"
+    width="100"
     alt="Logo"
     />
   )
