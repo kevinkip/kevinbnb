07b01e568de0067dba0a8446f1da7bc9a6f51ca6
Kevin Kipkemboi <theptjhub@gmail.com>
2023-06-30 15:08:59 -0700

    removed random listing

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -77,11 +77,7 @@ export default async function getListings(
                 }
             }
         }
-        const listingsCount = await prisma.listing.count();
-        const skip = Math.floor(Math.random() * listingsCount);
         const listings = await prisma.listing.findMany({
-            take: listingsCount,
-            skip: skip,
             where: query,
             orderBy: {
                 createdAt: 'desc'
