ac9d6cd74a34580c60e7a487eba1ca0963d26a73
Kevin Kipkemboi <theptjhub@gmail.com>
2023-06-30 20:00:24 -0700

    changed skiing to lighthouse

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -3,7 +3,7 @@
 import { usePathname, useSearchParams } from "next/navigation"
 import { TbBeach, TbMountain, TbPool } from 'react-icons/tb'
 import { GiBarn, GiBoatFishing, GiCactus, GiCastle, GiCaveEntrance, GiForestCamp, GiIsland, GiWindmill } from 'react-icons/gi'
-import { FaSkiing } from 'react-icons/fa'
+import { GiLighthouse } from 'react-icons/gi'
 import { BsSnow } from 'react-icons/bs'
 import { IoDiamond } from 'react-icons/io5'
 import { MdOutlineVilla } from 'react-icons/md'
@@ -48,8 +48,8 @@ export const categories = [
     description: 'This property is near a lake!'
   },
   {
-    label: 'Skiing',
-    icon: FaSkiing,
+    label: 'Lighthouse',
+    icon: GiLighthouse,
     description: 'This property has skiing activies!'
   },
   {
